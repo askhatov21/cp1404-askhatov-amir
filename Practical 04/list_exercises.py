10a916a78072c1d132827d15c097178df5018ab1
amiraskhatov21 <askhatov90@list.ru>
2025-06-14 13:29:54 +0800

    Added main function

---
+++ Practical 04/list_exercises.py
@@ -0,0 +1,21 @@
+def main():
+    """this function creates an empty list and prompts the user to enter 5 numbers"""
+numbers = []
+print("Please enter 5 numbers")
+
+"""Prompt user for 5 numbers and append each to the list"""
+for i in range(5):
+    number = int(input("Number: "))
+    numbers.append(number)
+
+
+
+
+
+
+
+
+
+
+
+main()
