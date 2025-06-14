be1dc2a152b115dbd1e3eb048903e0860d0f608b
amiraskhatov21 <askhatov90@list.ru>
2025-06-14 12:37:07 +0800

    changed the first element of numbers to ten

---
+++ Practical 04/lists_warmup.py
@@ -0,0 +1,8 @@
+# Change the first element of numbers to "ten" (the string, not the number 10)
+# Change the last element of numbers to 1
+# Print all the elements from numbers except the first two (slice)
+# Print whether 9 is an element of numbers
+
+
+
+numbers = ["ten", 1, 4, 1, 5, 9, 2]
