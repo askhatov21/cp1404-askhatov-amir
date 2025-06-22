b47853828d131a6192ac833ff0f72ead4424e6df
amiraskhatov21 <askhatov90@list.ru>
2025-06-22 09:59:12 +0800

    Add constant

---
+++ prac05_feedback/hex_colours.py
@@ -0,0 +1,4 @@
+COLOUR_NAMES = {"Absolute Zero": "#0048ba", "Acid Green": "#b0bf1a", "AliceBlue": "#f0f8ff", "Alizarin crimson": "#e32636",
+                "Amaranth": "#e52b50", "Amber": "ffbf00", "Amethyst": "#9966cc", "AntiqueWhite": "#faebd7", "AntiqueWhite1": "#faebd7",
+                "AntiqueWhite2": "#eedfcc" }
+print(COLOUR_NAMES)
