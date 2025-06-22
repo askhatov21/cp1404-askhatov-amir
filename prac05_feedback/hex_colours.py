075662f7f227092b6abe04f3355a81e05a661d93
amiraskhatov21 <askhatov90@list.ru>
2025-06-22 10:11:18 +0800

    Add constant Implement dictionary-based color hex code lookup Add interactive color hex converter using title-case handling Handle invalid color names with KeyError exception

--- prac05_feedback/hex_colours.py
+++ prac05_feedback/hex_colours.py
@@ -1,4 +1,16 @@
-COLOUR_NAMES = {"Absolute Zero": "#0048ba", "Acid Green": "#b0bf1a", "AliceBlue": "#f0f8ff", "Alizarin crimson": "#e32636",
+COLOUR_TO_HEX = {"Absolute Zero": "#0048ba", "Acid Green": "#b0bf1a", "AliceBlue": "#f0f8ff", "Alizarin crimson": "#e32636",
                 "Amaranth": "#e52b50", "Amber": "ffbf00", "Amethyst": "#9966cc", "AntiqueWhite": "#faebd7", "AntiqueWhite1": "#faebd7",
-                "AntiqueWhite2": "#eedfcc" }
-print(COLOUR_NAMES)
+                "AntiqueWhite2": "#eedfcc"}
+print(COLOUR_TO_HEX)
+
+color_name = input("Enter the name: ").strip()
+
+while color_name != "":
+    try:
+        """Convert input to case-intensive lookup(title casing the input"""
+        hex_code = COLOUR_TO_HEX[color_name.title()]
+        print(f'{color_name} is {hex_code}')
+    except KeyError:
+        print("Invalid color")
+    color_name = input("Enter the name: ").strip()
+print("Program excited")
