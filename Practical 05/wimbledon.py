cb94b326accc6afd555913027b4144304631ee7a
amiraskhatov21 <askhatov90@list.ru>
2025-06-22 18:38:47 +0800

    Add function to read data from Wimbledon file rocess champion win counts and participating countries Display champions and country statistics Extract champion and country from CSV row

--- Practical 05/wimbledon.py
+++ Practical 05/wimbledon.py
@@ -8,3 +8,25 @@ and display the results including win counts and number of countries"""
     display_results(champions_dict, countries_set)
 
 
+def read_data(filename):
+    """Read file and return list of data rows."""
+    champions_data = []
+    with open(filename, "r", encoding="utf-8-sig") as in_file:
+        for line in in_file:
+            champions_data.append(line.strip().split(","))
+    return champions_data
+
+
+def process_champions_data(champions_data):
+    """Count champion wins and collect countries."""
+    champions_dict = {}
+    countries_set = set()
+
+    for row in champions_data:
+        champion = row[2]
+        country = row[1]
+
+        countries_set.add(country)
+        champions_dict[champion] = champions_dict.get(champion, 0) + 1
+
+    return champions_dict, countries_set
