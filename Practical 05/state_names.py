2c0197b5f17d407d3fd39ce3df51ba24715507b3
amiraskhatov21 <askhatov90@list.ru>
2025-06-22 09:36:48 +0800

    Added loop to display state codes and their corresponding names Add interactive state code lookup with error handling

--- Practical 05/state_names.py
+++ Practical 05/state_names.py
@@ -4,7 +4,7 @@ State names in a dictionary
 File needs reformatting
 """
 
-# TODO: Reformat this file so the dictionary code follows PEP 8 convention
+
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania", "SA": "South Australia"}
 print(CODE_TO_NAME)
@@ -13,7 +13,7 @@ state_code = input("Enter short state: ")
 """Loop to display state codes and their corresponding names """
 for state_code, state_name in CODE_TO_NAME.items():
     print(f"{state_code} is {state_name}")
-""""""
+"""Look up full state names from short codes with error handling."""
 while state_code != "":
     try:
         print(state_code, "is", CODE_TO_NAME[state_code])
