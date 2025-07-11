f17fa3f5be0d495bbdc26c3987ed686268876cd1
amiraskhatov21 <askhatov90@list.ru>
2025-07-11 14:48:42 +0800

    the commit messages located in prac_06_feedback

---
+++ Practical_06/languages.py
@@ -0,0 +1,15 @@
+from programming_language import ProgrammingLanguage
+python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+print(python)
+
+# Create a list of languages
+languages = [python, ruby, visual_basic]
+
+#Print dynamically typed languages
+print("The dynamically typed languages are: ")
+for lang in languages:
+    if lang.is_dynamic():
+        print(lang.name)
