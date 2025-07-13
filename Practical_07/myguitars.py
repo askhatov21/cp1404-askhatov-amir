152326bbdffc6f3095a5618f8f0afa400c21a189
amiraskhatov21 <askhatov90@list.ru>
2025-07-13 14:34:41 +0800

    Add main program to display, sort, and add guitars using Guitar class
    Implement load_guitars() to read guitar data from CSV file
    Display existing guitars, sort them by year using __lt__ method
    Prompt user to input new guitars and save them back to the file
    Use modular functions for loading and saving
    Fix indentation, typos, and improve readability
    Add save_guitar_to_file() to append new guitars to CSV file

--- Practical_07/myguitars.py
+++ Practical_07/myguitars.py
@@ -1,38 +1,45 @@
-from Practical_07 import
+from Practical_07 import Guitar
+
 FILENAME = 'guitars.csv'
 
+
 def main():
-    """Display existing guitars and prompt user for new guitar to add and display"""
-  guitars = load_guitars()
+    """Display existing guitars, allow user to add more, then save to file."""
+    guitars = load_guitars()
 
-  print("These are the existing guitars: ")
-  for guitar in guitars:
-      print(guitar)
+    print("These are the existing guitars:")
+    for guitar in guitars:
+        print(guitar)
 
-  print()
-  guitars.sort() # Sort by year (if Guitar has a method __lt__)
-  print("Sorted guitars:")
-  for guitar in guitars:
-      print(guitar)
+    print()
+    guitars.sort()  # Sort by year( if Guitar has a method __lt__)
+    print("Sorted guitars:")
+    for guitar in guitars:
+        print(guitar)
 
-    #Add a new guitars
+    # Add a new guitars
     name = input("Name: ")
     while name != "":
         year = int(input("Year: "))
         cost = float(input("Cost: $"))
-        new_guitar = Guitar(name, year, cost)
+        new_guitar = load_guitars(name, year, cost)
         guitars.append(new_guitar)
         save_guitar_to_file(new_guitar)
         name = input("Name: ")
 
-
 def load_guitars():
-    """Load guitars from file nad return a list of guitar objects"""
+    """Load guitars from file and return a list of Guitar objects."""
     guitars = []
     with open(FILENAME, 'r') as in_file:
         for line in in_file:
             name, year, cost = line.strip().split(',')
-            guitar = Guitar(name, int(year), float(cost))
+            guitar = load_guitars(name, int(year), float(cost))
             guitars.append(guitar)
-        return guitars
+    return guitars
+
+def save_guitar_to_file(guitar):
+    """Append a new guitar to the CSV file."""
+    with open(FILENAME, 'a') as out_file:
+        print(f"{guitar.name},{guitar.year},{guitar.cost}", file=out_file)
+
 main()
