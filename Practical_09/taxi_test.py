91dc34d3168972aee7cf1f3e06c7f0b3eb7e5952
amiraskhatov21 <askhatov90@list.ru>
2025-07-27 10:41:46 +0800

    Create taxi_test.py to test Taxi class functionality.

---
+++ Practical_09/taxi_test.py
@@ -0,0 +1,19 @@
+from taxi import Taxi
+# Step 1: Create the taxi object
+my_taxi = Taxi("Prius 1", fuel=100, price_per_km=1.23)
+
+# Step 2: Start a new fare Taxi object
+my_taxi.start_fare()
+my_taxi.drive(40)
+
+# Step 3: Print the taxi's details and fare
+print(my_taxi)
+print(f"Current fare: ${my_taxi.get_fare()}:.2f")
+
+# Step 4: Start a new fare and drive 100 km
+my_taxi.start_fare()
+my_taxi.drive(100)
+
+# Step 5: Print the taxi's details and fare
+print(my_taxi)
+print(f"Current fare: ${my_taxi.get_fare()}:.2f")
